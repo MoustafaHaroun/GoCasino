fd4ded35edaae5818844f179575b6e6198b9cc08
MoustafaHaroun <Moustafa_Haroun@outlook.com>
2024-07-09 19:49:47 +0200

    feat(server): add listing and connection handling.

--- server/main.go
+++ server/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"log"
+	"net"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// Start listing on port 8080
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting: ", err.Error())
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	request, _ := http.ReadRequest(reader)
+
+	response, err := handshake(request)
+	if err != nil {
+		log.Println(err)
+		conn.Close()
+	}
+
+	conn.Write([]byte(response))
 }
